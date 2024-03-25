d587d08e49f34f64ae01b8127f0457a29574f00f
mateorodriguez <a01572463@tec.mx>
2024-03-25 17:48:48 -0600

    Progress made on Chassis subsystem File for Chassis command created (MoveChassis)

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/teleOp/subsystems/Chassis.java
@@ -0,0 +1,51 @@
+package org.firstinspires.ftc.teamcode.teleOp.subsystems;
+
+
+import com.arcrobotics.ftclib.command.SubsystemBase;
+import com.arcrobotics.ftclib.geometry.Rotation2d;
+import com.arcrobotics.ftclib.kinematics.DifferentialOdometry;
+import com.arcrobotics.ftclib.kinematics.wpilibkinematics.DifferentialDriveOdometry;
+import com.qualcomm.hardware.rev.RevHubOrientationOnRobot;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.DcMotorEx;
+import com.qualcomm.robotcore.hardware.DcMotorSimple;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.IMU;
+
+public class Chassis extends SubsystemBase {
+    //Motor declaration
+    private DcMotorEx left_Drive;
+    private DcMotorEx right_Drive;
+
+    private final double M_PER_TICK = 1.0 / 54000.0 * 9.0 * Math.PI;
+    static final double TRACKWIDTH = 1; //space that wheels take
+
+    private DifferentialDriveOdometry diffOdom;
+
+    private IMU imu;
+
+    public Chassis(HardwareMap hardwareMap) {
+        //Motor ID
+        left_Drive = (DcMotorEx) hardwareMap.get(DcMotor.class, "left_Drive");
+        right_Drive = (DcMotorEx) hardwareMap.get(DcMotor.class, "right_Drive");
+
+        //Invert left motor
+        left_Drive.setDirection(DcMotor.Direction.REVERSE);
+        right_Drive.setDirection(DcMotor.Direction.FORWARD);
+
+        //Initialize odometry and IMU
+        diffOdom = new DifferentialDriveOdometry(new Rotation2d());
+        imu = hardwareMap.get(IMU.class, "imu");
+
+        //Where the IMU is facing and where it is located
+        IMU.Parameters imuParameters = new IMU.Parameters(
+                new RevHubOrientationOnRobot(RevHubOrientationOnRobot.LogoFacingDirection.BACKWARD,
+                        RevHubOrientationOnRobot.UsbFacingDirection.UP)
+        );
+
+    }
+
+
+}
+
+
